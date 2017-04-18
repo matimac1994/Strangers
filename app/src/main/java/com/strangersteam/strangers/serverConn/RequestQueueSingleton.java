87c0858b35ef723623cdc6b705f4bd5a875c5ce8
Krolis <lolosiowo2>
2017-04-18 17:19:35 +0200

    login - serwer connection

---
+++ app/src/main/java/com/strangersteam/strangers/serverConn/RequestQueueSingleton.java
@@ -0,0 +1,43 @@
+package com.strangersteam.strangers.serverConn;
+
+import android.content.Context;
+
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.toolbox.Volley;
+
+public class RequestQueueSingleton {
+    private static RequestQueueSingleton mInstance;
+    private RequestQueue mRequestQueue;
+    private static Context mCtx;
+
+    public RequestQueueSingleton(Context context) {
+        mCtx = context;
+        mRequestQueue = getRequestQueue();
+
+    }
+
+    /**
+     *
+     * @param context should be Application context, NOT Activity context. Example: RequestQueueSingleton.getInstance(this.getApplicationContext())
+     * @return RequestQueueSingleton instance
+     */
+    public static synchronized RequestQueueSingleton getInstance(Context context) {
+        if (mInstance == null) {
+            mInstance = new RequestQueueSingleton(context);
+        }
+        return mInstance;
+    }
+
+    public RequestQueue getRequestQueue() {
+        if (mRequestQueue == null) {
+            mRequestQueue = Volley.newRequestQueue(mCtx.getApplicationContext());
+        }
+        return mRequestQueue;
+    }
+
+    public <T> void addToRequestQueue(Request<T> req) {
+        getRequestQueue().add(req);
+    }
+
+}
---
+++ app/src/main/java/com/strangersteam/strangers/serverConn/ServerConfig.java
@@ -0,0 +1,6 @@
+package com.strangersteam.strangers.serverConn;
+
+public final class ServerConfig {
+    public static final String SERVER_IP = "http://10.0.3.2:9997/";
+    public static final String LOGIN = SERVER_IP + "user/session/login";
+}
